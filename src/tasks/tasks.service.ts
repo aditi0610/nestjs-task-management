759aeb183e26605d1547d9bbad14df38b1158687
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-14 18:26:09 +0530

    Create a Task Service

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,6 @@
+//service will be called from a controller, usually to validate data, create an item in the database,
+//  and maybe even return a response that is complicated and needs to be calculated or mapped 
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TasksService {}
