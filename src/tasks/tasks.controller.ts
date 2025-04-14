386081327908da45179434e39d32764bd47b9fef
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-14 18:58:39 +0530

    add GET endpoint to retrieve all tasks from TasksService

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,17 @@
 // controller decorator accepts a string, which is the path to be handled by the controller
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { get } from 'http';
 
 @Controller('tasks')
 export class TasksController 
 {
     constructor(private tasksService: TasksService){ } // TasksService ko constructor ke through use karne ke liye inject kiya gaya hai
 
-
+    @Get()
+      getAllTasks(){
+        // return 'Hello';
+        return this.tasksService.getAllTasks();
+      }
    
 }
