24251f88850e754dbdef3db114da31e529715efd
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-15 11:16:33 +0530

    update POST endpoint to use CreateTaskDto for task creation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,6 +2,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController 
@@ -16,11 +17,8 @@ export class TasksController
 
       //add POST endpoint to create a new task using TasksService
       @Post()
-       createTask(
-        @Body('title') title: string,
-        @Body('description') description: string,
-       ): Task {
-           return this.tasksService.createTask(title, description);
+       createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
+           return this.tasksService.createTask(CreateTaskDto);
        }
         
    
