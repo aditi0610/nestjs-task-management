8b08d5de9408747d3a5e70741f354b09e22441aa
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-15 10:47:39 +0530

    implement createTask method to add new task with UUID and default status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,13 +1,29 @@
 //service will be called from a controller, usually to validate data, create an item in the database,
 //  and maybe even return a response that is complicated and needs to be calculated or mapped 
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
     private tasks: Task[] = [];  //only this class can access or modify it.
 
-    getAllTasks(): Task[] {
+    getAllTasks(): Task[] 
+    {
         return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task 
+    {
+        const task: Task = 
+        {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        }
+
+        this.tasks.push(task);
+        return task;
     }
 }
