7e46e1ba0feae5a8688798118422baafb19de6b5
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-15 10:48:38 +0530

    add POST endpoint to create a new task using TasksService

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,6 @@
 // controller decorator accepts a string, which is the path to be handled by the controller
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { get } from 'http';
 import { Task } from './task.model';
 
 @Controller('tasks')
@@ -14,5 +13,14 @@ export class TasksController
         // return 'Hello';
         return this.tasksService.getAllTasks();
       }
+
+      @Post()
+       createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+       ): Task {
+           return this.tasksService.createTask(title, description);
+       }
+        
    
 }
